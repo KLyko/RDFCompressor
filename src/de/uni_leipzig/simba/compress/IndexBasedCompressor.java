42b66129fd437383a4ec3a53ea89fb5195810d8f
Klaus Lyko <lyko.klaus@gmail.com>
2014-02-26 14:06:18 +0100

    switching HashMaps, test works

--- src/de/uni_leipzig/simba/compress/IndexBasedCompressor.java
+++ src/de/uni_leipzig/simba/compress/IndexBasedCompressor.java
@@ -2,7 +2,6 @@ package de.uni_leipzig.simba.compress;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -17,7 +16,6 @@ import java.util.Map.Entry;
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorOutputStream;
 import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
-import org.apache.commons.compress.utils.IOUtils;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.Statement;
@@ -37,10 +35,9 @@ import de.uni_leipzig.simba.data.IndexRule;
 public class IndexBasedCompressor implements Compressor, IndexBasedCompressorInterface {
 	
 	HashMap<String, String> shortToUri = new HashMap();
-	HashMap<Integer, String> subjectMap = new HashMap();
-	HashMap<Integer, String> objectMap = new HashMap();
-	HashMap<Integer, String> propertyMap = new HashMap();
-
+	HashMap<String, Integer> subjectMap = new HashMap();
+	HashMap<String, Integer> objectMap = new HashMap();
+	HashMap<String, Integer> propertyMap = new HashMap();
 	public IndexBasedCompressor() {
 		//nothing to do here so far.
 	}
@@ -176,10 +173,10 @@ public class IndexBasedCompressor implements Compressor, IndexBasedCompressorInt
 			    // write subject index
 			    outputStream = new ByteArrayOutputStream( );
 
-			    for (Entry<Integer, String> subject : this.subjectMap.entrySet()) {
-			    	outputStream.write( subject.getKey().toString().getBytes());
+			    for (Entry<String, Integer> subject : this.subjectMap.entrySet()) {
+			    	outputStream.write( subject.getKey().getBytes());
 			    	outputStream.write( "|".getBytes());
-			    	outputStream.write( subject.getValue().getBytes());
+			    	outputStream.write( subject.getValue().toString().getBytes());
 			    	outputStream.write( "\n".getBytes());
 			    }
 			    byte subjects[] = outputStream.toByteArray( );
@@ -193,10 +190,10 @@ public class IndexBasedCompressor implements Compressor, IndexBasedCompressorInt
 			    // write object index
 			    outputStream = new ByteArrayOutputStream( );
 
-			    for (Entry<Integer, String> object : this.objectMap.entrySet()) {
-			    	outputStream.write( object.getKey().toString().getBytes());
+			    for (Entry<String, Integer> object : this.objectMap.entrySet()) {
+			    	outputStream.write( object.getKey().getBytes());
 			    	outputStream.write( "|".getBytes());
-			    	outputStream.write( object.getValue().getBytes());
+			    	outputStream.write( object.getValue().toString().getBytes());
 			    	outputStream.write( "\n".getBytes());
 			    }
 			    byte objects[] = outputStream.toByteArray( );
@@ -210,10 +207,10 @@ public class IndexBasedCompressor implements Compressor, IndexBasedCompressorInt
 			    // write property index
 			    outputStream = new ByteArrayOutputStream( );
 
-			    for (Entry<Integer, String> property : this.propertyMap.entrySet()) {
-			    	outputStream.write( property.getKey().toString().getBytes());
+			    for (Entry<String, Integer> property : this.propertyMap.entrySet()) {
+			    	outputStream.write( property.getKey().getBytes());
 			    	outputStream.write( "|".getBytes());
-			    	outputStream.write( property.getValue().getBytes());
+			    	outputStream.write( property.getValue().toString().getBytes());
 			    	outputStream.write( "\n".getBytes());
 			    }
 			    byte properties[] = outputStream.toByteArray( );
@@ -292,59 +289,41 @@ public class IndexBasedCompressor implements Compressor, IndexBasedCompressorInt
 		int index =-1;
 		switch(SPOrO) {
 			case SUBJECT: 
-				if(subjectMap.containsValue(uri)) {
-					for(Entry<Integer, String> e:subjectMap.entrySet())
-						if(e.getValue().equals(uri)) {
-							index = e.getKey();
+				if(subjectMap.containsKey(uri)) {
+					index = subjectMap.get(uri);
 							break;
-						}	
-				} else { // create new one
+				}	
+				else { // create new one
 					index = subjectMap.size();
-					subjectMap.put(subjectMap.size(), uri);
-				}				
+					subjectMap.put(uri, index);
+				}
 				break;
 			case PREDICATE: 
-				if(propertyMap.containsValue(uri)) {
-					for(Entry<Integer, String> e:propertyMap.entrySet())
-						if(e.getValue().equals(uri)) {
-							index = e.getKey();
+				if(propertyMap.containsKey(uri)) {
+					index = propertyMap.get(uri);
 							break;
-						}	
-				} else { // create new one
+				}	
+				else { // create new one
 					index = propertyMap.size();
-					propertyMap.put(propertyMap.size(), uri);
+					propertyMap.put(uri, propertyMap.size());
 				}				
 				break;
 			case OBJECT:
-				if(objectMap.containsValue(uri)) {
-					for(Entry<Integer, String> e:objectMap.entrySet())
-						if(e.getValue().equals(uri)) {
-							index = e.getKey();
-							break;
-						}	
+				if(objectMap.containsKey(uri)) {
+					index = objectMap.get(uri);
+					break;
 				} else { // check for subjects
-					if(subjectMap.containsValue(uri)) {
-						int subjectIndex = -1;
-						for(Entry<Integer, String> e:subjectMap.entrySet())
-							if(e.getValue().equals(uri)) {
-								subjectIndex = e.getKey();
-								break;
-							}
-						if(objectMap.containsValue(""+subjectIndex)) {
-							for(Entry<Integer, String> e:objectMap.entrySet())
-								if(e.getValue().equals(""+subjectIndex)) {
-									index = e.getKey();
-									break;
-								}	
-						} else { // create new one
-							index = objectMap.size();
-							objectMap.put(objectMap.size(), ""+subjectIndex);
-						}				
+					if(subjectMap.containsKey(uri)) {
+						uri = ""+subjectMap.get(uri);
+					}
+					if(objectMap.containsKey(uri)) {
+						index = objectMap.get(uri);
 						break;
-					} //also subject doesn#T exists 
-					index = objectMap.size();
-					objectMap.put(objectMap.size(), uri);
-				}				
+					}else {
+						index = objectMap.size();
+						objectMap.put(uri, index);
+					}
+				}		
 				break;
 		}
 		return index;
